fdffb565d91748422ab24dd4e83a215c86dc6853
lelana0824 <lelana0824@gmail.com>
2022-04-02 21:54:26 +0900

    feat: 선택한 말 테두리 색칠 + 클릭시 오디오 재생

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ canvas.style.height = `${width}px`;
 canvas.width = width * dpr;
 canvas.height = height * dpr;
 
+// 오디오 모음
+const clickSound = new Audio('../static/click-sound.mp3');
+
 // 체스판 그리기
 const boardWithPieces = []
 const diedPieces = [];
@@ -146,9 +149,6 @@ canvas.addEventListener('click', (e) => {
     const dx = mouseX * dpr
     const dy = mouseY * dpr
 
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    ctx.fillRect(dx, dy, 10, 10);
-
     const clickedBlock = boardWithPieces.find((block) => {
         const blockPosition = block[0];
 
@@ -159,11 +159,36 @@ canvas.addEventListener('click', (e) => {
 
     if (clickedBlock[1]) {
         // 말을 선택한 경우
+        clickSound.play();
+
+        ctx.strokeStyle = '#bfff00'
+        ctx.lineWidth = 10;
+        ctx.strokeRect(...(clickedBlock[0].map((position, index) => {
+            if (index === 2 || index === 3) {
+                return position - 20
+            }
+            return position + 10
+        })))
+
         if (selected) {
             // 이전에 선택한 말이 존재할 경우
             if (selected[1].team === clickedBlock[1].team) {
                 // 이전과 지금 선택한 말이 같은 진영일 경우 그냥 리턴
-                selected = null;
+                ctx.strokeStyle = selected[0][selected[0].length - 1];
+                ctx.strokeRect(...(selected[0].map((position, index) => {
+                    if (index === 2 || index === 3) {
+                        return position - 20
+                    }
+                    return position + 10
+                })))
+                
+                if (selected[1] === clickedBlock[1]) {
+                    // 같은 말을 한번 더 클릭한 경우라면 선택 취소하고 리턴
+                    selected = null;
+                    return;
+                }
+
+                selected = clickedBlock;
                 return
             }
             diedPieces.push(clickedBlock[1]);
