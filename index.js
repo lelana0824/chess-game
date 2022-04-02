eb847c9393890392372dda8f46bd2f9f725d2bc0
lelana0824 <lelana0824@gmail.com>
2022-04-02 10:51:06 +0900

    feat: 체스판 색 변경

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ canvas.height = height * dpr;
 
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-        ctx.fillStyle = ((i + j) % 2 === 0) ? 'gray' : 'green';
+        ctx.fillStyle = ((i + j) % 2 === 0) ? '#fece9e' : '#d18a48';
         ctx.fillRect(200 * j, 200 * i, 100 * dpr, 100 * dpr);
     }
 }
