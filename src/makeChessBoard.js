3078f73d9d39bdb80645c902795caf0a45ef5288
lelana0824 <lelana0824@gmail.com>
2022-04-16 21:31:43 +0900

    feat: piece 객체에 movehistory 배열 추가

--- src/makeChessBoard.js
+++ src/makeChessBoard.js
@@ -33,12 +33,14 @@ function initChessBoard(board) {
                     name: 'PAWN',
                     team: 'BLACK',
                     position: [1000, 200, 200, 200],
+                    moveHistory: [],
                 };
             } else if (i === 6) {
                 piece = {
                     name: 'PAWN',
                     team: 'WHITE',
                     position: [1000, 0, 200, 200],
+                    moveHistory: [],
                 };
 
                 // 양쪽 룩 그리기
@@ -47,12 +49,14 @@ function initChessBoard(board) {
                     name: 'ROOK',
                     team: 'BLACK',
                     position: [800, 200, 200, 200],
+                    moveHistory: [],
                 };
             } else if (i === 7 && (j === 0 || j === 7)) {
                 piece = {
                     name: 'ROOK',
                     team: 'WHITE',
                     position: [800, 0, 200, 200],
+                    moveHistory: [],
                 };
 
                 // 양쪽 나이트 그리기
@@ -61,12 +65,14 @@ function initChessBoard(board) {
                     name: 'KNIGHT',
                     team: 'BLACK',
                     position: [600, 200, 200, 200],
+                    moveHistory: [],
                 };
             } else if (i === 7 && (j === 1 || j === 6)) {
                 piece = {
                     name: 'KNIGHT',
                     team: 'WHITE',
                     position: [600, 0, 200, 200],
+                    moveHistory: [],
                 };
 
                 // 양쪽 비숍 그리기
@@ -75,36 +81,42 @@ function initChessBoard(board) {
                     name: 'BISHOP',
                     team: 'BLACK',
                     position: [400, 200, 200, 200],
+                    moveHistory: [],
                 };
             } else if (i === 7 && (j === 2 || j === 5)) {
                 piece = {
                     name: 'BISHOP',
                     team: 'WHITE',
                     position: [400, 0, 200, 200],
+                    moveHistory: [],
                 };
             } else if (i === 0 && j === 3) {
                 piece = {
                     name: 'KING',
                     team: 'BLACK',
                     position: [200, 200, 200, 200],
+                    moveHistory: [],
                 };
             } else if (i === 7 && j === 3) {
                 piece = {
                     name: 'KING',
                     team: 'WHITE',
                     position: [200, 0, 200, 200],
+                    moveHistory: [],
                 };
             } else if (i === 0 && j === 4) {
                 piece = {
                     name: 'QUEEN',
                     team: 'BLACK',
                     position: [0, 200, 200, 200],
+                    moveHistory: [],
                 };
             } else if (i === 7 && j === 4) {
                 piece = {
                     name: 'QUEEN',
                     team: 'WHITE',
                     position: [0, 0, 200, 200],
+                    moveHistory: [],
                 };
             }
 
